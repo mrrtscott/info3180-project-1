e5287cdde4ca10f5a8ff23a6f4f85323a4e1bb52
Scott <scott.r.ja@gmail.com>
2020-04-11 01:14:35 -0500

    database migration versions

--- app/__init__.py
+++ app/__init__.py
@@ -7,17 +7,21 @@ UPLOAD_FOLDER = './app/static/uploads'
 
 app = Flask(__name__)
 
+ENV = 'dev'
 
-
-app.debug = True
-app.config['SECRET_KEY'] = "dgvfjsdhgkh4755"
-app.config['SQLALCHEMY_DATABASE_URI'] = "postgresql://fmtesiqeektnzz:67a23e7a9b24a1e53e068a19231c8909ec0286a74ae6ef05e4488fcc5af673fd@ec2-34-204-22-76.compute-1.amazonaws.com:5432/dat05mgb5m0o7o"
+if ENV == 'dev':
+    app.debug = True
+    app.config['SECRET_KEY'] = "dgvfjsdhgkh4755"
+    app.config['SQLALCHEMY_DATABASE_URI'] = "postgresql://project1:uwicomputing@localhost/project"
     
 
-app.debug = False
+else:
+
+    app.debug = False
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True # added just to suppress a warning
+
+    
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True # added just to suppress a warning
-
-
 
 
 
