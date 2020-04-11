e9113a8fa946f886ca460bf205b0f77ffd56bb7e
Scott <scott.r.ja@gmail.com>
2020-04-11 01:52:06 -0500

    new migration

--- app/__init__.py
+++ app/__init__.py
@@ -12,7 +12,7 @@ ENV = 'dev'
 if ENV == 'dev':
     app.debug = True
     app.config['SECRET_KEY'] = "dgvfjsdhgkh4755"
-    app.config['SQLALCHEMY_DATABASE_URI'] = "postgresql://fmtesiqeektnzz:67a23e7a9b24a1e53e068a19231c8909ec0286a74ae6ef05e4488fcc5af673fd@ec2-34-204-22-76.compute-1.amazonaws.com:5432/dat05mgb5m0o7o"
+    app.config['SQLALCHEMY_DATABASE_URI'] = "postgresql://project1:uwicomputing@localhost/project"
     
 
 else:
